3dfb2f72bdc5d1ae6fc18bbe2e6a34bb4c8525d9
PivotalSharedIreland <fmangione@pivotal.io>
2016-01-15 09:33:28 +0000

    Initial Commit

---
+++ SwiftList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftList
+//
+//  Created by Pivotal on 15/01/2016.
+//  Copyright © 2016 Pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
